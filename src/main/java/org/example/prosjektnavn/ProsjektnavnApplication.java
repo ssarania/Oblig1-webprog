e2adfff8f57af7e647d9df7a37e1cfde1e567e59
ssarania <sarania0306@gmail.com>
2024-02-07 16:04:47 +0100

    inital commit

---
+++ src/main/java/org/example/prosjektnavn/ProsjektnavnApplication.java
@@ -0,0 +1,13 @@
+package org.example.prosjektnavn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProsjektnavnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProsjektnavnApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/static/script.js
@@ -0,0 +1,48 @@
+document.getElementById("kjopBillett").addEventListener("click", validateAndLeggTilBillett);
+document.getElementById("slettAlleBilletter").addEventListener("click", slettAlleBilletter);
+
+function validateAndLeggTilBillett() {
+    const film = document.getElementById("filmValg").value;
+    const antall = document.getElementById("antallBilletter").value;
+    const fornavn = document.getElementById("fornavn").value;
+    const etternavn = document.getElementById("etternavn").value;
+    const telefonr = document.getElementById("telefonr").value;
+    const epost = document.getElementById("epost").value;
+
+    var telefonRegex = /^((0047)?|\+47|47)?\d{8}$/;
+    var epostRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    if (film !== "" && antall !== "" && fornavn !== "" && etternavn !== "" && telefonr !== "" && epost !== "") {
+        leggTilBillett();
+    } else if (!telefonRegex.test(telefonr)) {
+        alert("Vennligst oppgi et gyldig telefonnummer med 8 siffer.");
+    } else if (!epostRegex.test(epost)){
+        alert("Vennligst oppgi et gyldig epost med @.");
+    } else {
+        alert("Vennligst fyll ut alle feltene.");
+    }
+}
+function leggTilBillett() {
+    const film = document.getElementById("filmValg").value;
+    const antall = document.getElementById("antallBilletter").value;
+    const fornavn = document.getElementById("fornavn").value;
+    const etternavn = document.getElementById("etternavn").value;
+    const telefonr = document.getElementById("telefonr").value;
+    const epost = document.getElementById("epost").value;
+
+    const nyBillett = document.createElement("li");
+    nyBillett.textContent = `${film} - ${antall} billetter, ${fornavn} ${etternavn}, ${telefonr}, ${epost}`;
+
+    document.getElementById("billettListe").appendChild(nyBillett);
+
+    document.getElementById("filmValg").value = "";
+    document.getElementById("antallBilletter").value = "";
+    document.getElementById("fornavn").value = "";
+    document.getElementById("etternavn").value = "";
+    document.getElementById("telefonr").value = "";
+    document.getElementById("epost").value = "";
+}
+
+function slettAlleBilletter() {
+    document.getElementById("billettListe").innerHTML = "";
+}
